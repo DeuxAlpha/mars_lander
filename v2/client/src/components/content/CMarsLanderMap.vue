3249fe942f791102a9ea068c1746c710cc76f3fc
Aaron Passchier <aaron.passchier@bissell.com>
2020-06-27 15:38:36 -0400

    Basic graph is working

--- v2/client/src/components/content/CMarsLanderMap.vue
+++ v2/client/src/components/content/CMarsLanderMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="map" id="map-wrapper" class="w-full h-full"/>
+  <div ref="map" id="map-wrapper"/>
 </template>
 
 <script lang="ts">
@@ -8,18 +8,15 @@
 
   @Component
   export default class CMarsLanderMap extends Vue {
-    @Ref('map') readonly svgElement!: SVGSVGElement;
-    controller!: MarsLanderMapController;
+    @Ref('map') readonly mapWrapper!: HTMLElement;
+    controller: MarsLanderMapController = new MarsLanderMapController();
 
     mounted() {
-      this.controller = new MarsLanderMapController(this.svgElement);
+      this.controller.Init(this.mapWrapper);
       this.controller.GenerateBoundaries(7000, 3000);
     }
   }
 </script>
 
 <style scoped lang="scss">
-  #map-wrapper {
-    background: red;
-  }
 </style>
--- v2/client/src/components/content/MarsLanderMapController.ts
+++ v2/client/src/components/content/MarsLanderMapController.ts
@@ -1,20 +1,29 @@
 import * as D3 from 'd3';
 
 export class MarsLanderMapController {
-  constructor(element: SVGSVGElement) {
-    this.map = D3.select(element);
-    this.buildSvg();
-  }
+  private wrapper!: HTMLElement;
+  private map!: D3.Selection<SVGSVGElement, any, null, undefined>;
 
-  private readonly map!: D3.Selection<SVGSVGElement, any, null, undefined>;
-
-  private buildSvg() {
-    this.map.append('svg')
-      .attr('width', '100%')
-      .attr('height', '100%');
+  public Init(element: HTMLElement) {
+    this.wrapper = element;
   }
 
   public GenerateBoundaries(x: number, y: number) {
+    console.dir(this.wrapper);
+    this.map = D3.select(this.wrapper)
+      .append('svg')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .attr('viewBox', '0 0 7000 3000')
 
+    const data = [{x: 0, y: 20}, {x: 150, y: 150}, {x: 300, y: 100}, {x: 450, y: 20}, {x: 600, y: 130}]
+
+    this.map.append('path')
+      .datum(data)
+      .attr('d', D3.line<{x: number, y: number}>()
+        .x(d => d.x)
+        .y(d => d.y))
+      .attr('stroke', 'black')
+      .attr('fill', 'none');
   }
 }
